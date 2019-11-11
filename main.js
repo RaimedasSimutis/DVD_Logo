5d322c5d3cd4b133030ad5d6e42686c91441243a
Raimedas Simutis <raimedas.simutis@gmail.com>
2019-11-11 19:25:59 +0200

    readme file update

--- main.js
+++ main.js
@@ -1,33 +1,13 @@
 const logo = document.querySelector(".logo");
 const container = document.querySelector(".container");
-const logoW = logo.offsetWidth;
-const logoH = logo.offsetHeight;
-let logoX = logo.offsetLeft;
-let logoY = logo.offsetTop;
-const containerW = container.offsetWidth;
-const containerH = container.offsetHeight;
-const animSpeed = 1;
-let filteredColor = "invert(48%) sepia(94%) saturate(1397%) brightness(90%) contrast(101%)"
-
-// console.log(logo);
-// console.log(logoW);
-// console.log(logoH);
-// console.log(logoX);
-// console.log(logoY);
-// console.log(container);
-// console.log(containerW);
-// console.log(containerH);
-
-//logo.style.left = "255px"
-
-
+let filteredColor = "invert(48%) sepia(94%) saturate(1397%) brightness(90%) contrast(101%)";
 
 let directionX;
 let directionY;
-let speedX = 1;
-let speedY = 1;
+const speedX = 1;
+const speedY = 1;
 
-var iteratorX = function (index) {
+let iteratorX = function (index) {
     if (index >= container.offsetWidth - logo.offsetWidth) {
         //index = 0;
         directionX = -1;
@@ -48,7 +28,7 @@ var iteratorX = function (index) {
     }, 10);
 };
 
-var iteratorY = function (index) {
+let iteratorY = function (index) {
     if (index >= container.offsetHeight - logo.offsetHeight) {
         //index = 0;
         directionY = -1;
